2057d8ae5a8a7062afde67d6e99159d6c0455131
Elco Jacobs <elcojacobs@gmail.com>
2018-07-09 14:54:25 +0200

    generate base64 png url with npm package

---
+++ .vuepress/components/PlantUml.vue
@@ -0,0 +1,31 @@
+<template>
+<p>
+    <img :src="url" :alt="title" :title="title">
+</p>
+</template>
+
+<script>
+const plantumlEncoder = require('plantuml-encoder')
+
+module.exports = {
+    props: {
+        src: String,
+        title: String,
+    },
+    computed: {
+        url: function() {
+            // console.log(this.src)
+            if(this.src){
+                const sourceFile = require("../../" + this.src);
+                console.log(sourceFile)
+                const encoded = plantumlEncoder.encode(sourceFile);
+                console.log(encoded) 
+                return "http://www.plantuml.com/plantuml/png/" + encoded;
+            }
+            else {
+                return "";
+            }
+        }
+    }
+}
+</script>
