215c79827ae4df29fb2dbe7c33dbeec867bc09f5
Bob Steers <steers.bob@gmail.com>
2023-11-29 15:38:08 +0100

    dependency updates

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,20 @@
 const plantuml = require('./plantuml');
+const crypto = require('crypto');
+
+/**
+ * From: https://stackoverflow.com/a/72219174
+ *
+ * The MD4 algorithm is not available anymore in Node.js 17+ (because of library SSL 3).
+ * In that case, silently replace MD4 by the MD5 algorithm.
+ */
+try {
+  crypto.createHash('md4');
+} catch (e) {
+  const origCreateHash = crypto.createHash;
+  crypto.createHash = (alg, opts) => {
+    return origCreateHash(alg === 'md4' ? 'md5' : alg, opts);
+  };
+}
 
 module.exports = {
   title: 'Brewblox',
@@ -53,6 +69,7 @@ module.exports = {
         {
           title: 'Using brewblox-service',
           children: [
+            'service/python_env',
             'service/architecture',
             'service/profiling',
           ],
